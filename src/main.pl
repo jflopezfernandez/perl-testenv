4721d83a1d7a0bc4599d5e66247fc2c22e27c552
lopezfjose <jflopezfernandez@gmail.com>
2018-11-23 18:14:08 -0500

    Exception handling

--- src/main.pl
+++ src/main.pl
@@ -5,3 +5,14 @@ use v5.26;
 
 use strict;
 use warnings;
+
+
+my $input = "";
+
+do {
+    chomp($input = <STDIN>);
+
+    my $len = length($input);
+
+    say "Length: $len" if $input;
+} while ($input);
